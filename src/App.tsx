1dd24ec2d1715ffa3edfb57d77fd38f4abb8b689
Simon Benjámin <benzsi0105@gmail.com>
2026-03-18 23:23:44 +0100

    fix: Comment out missing AppLists.css import to resolve loading error

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
-import './AppLists.css'
+// import './AppLists.css' // File missing; import commented out to resolve error
 import { Header } from './components/Header'
 import { Home } from './pages/Home'
 import { Login } from './pages/Login'
